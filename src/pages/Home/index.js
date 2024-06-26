d300662c2e2565e075d18ed6c34737a3db0a792b
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-26 00:01:50 -0400

    new

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react";
+import api from '../../services/api'
+
+// https://api.themoviedb.org/3/movie/now_playing?api_key=512f6c5c7620fb7821612a4adebfa5f6&language=en-US
 
 function Home() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+    
+    async function loadFilmes(){
+        const response = await api.get("movie/now_playing", {
+            params:{
+                api_key:"512f6c5c7620fb7821612a4adebfa5f6",
+                page: 1,
+            }
+        })
+
+        //console.log(response.data.results.slice(0, 10));
+        setFilmes(response.data.results.slice(0, 10))
+    
+    }
+
+    loadFilmes();
+
+}, [])
+
     return (
-        <div>
-            <h1>Welcome to Home!</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                        </article>
+                    )
+                } )}
+            </div>
         </div>
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// BASE DA URL 
-// URL DA API 
+// BASE DA URL https://api.themoviedb.org/3
+// URL DA APIhttps://api.themoviedb.org/3/movie/now_playing?api_key=512f6c5c7620fb7821612a4adebfa5f6&language=en-US
 
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
