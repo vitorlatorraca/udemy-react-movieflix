b8bc3abd73eafdc4f55507471ad4c780faeef261
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-27 23:39:22 -0400

    new

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,28 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import api from '../../services/api'
 
 function Filme() {
-    const { id } = useParams(); 
+    const { id } = useParams();
+    
+    useEffect(() => {  // Correção aqui: fechamento correto do useEffect
+        async function loadFilme() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "512f6c5c7620fb7821612a4adebfa5f6"
+                }
+            })
+            .then((response) => {
+                console.log(response);
+            })
+            .catch(() => {
+                console.log("Erro ao carregar o filme");
+            })
+        }
 
+        loadFilme();  
+    }, []);  
 
     return (
         <div>
